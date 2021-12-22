76caf936776032d518ce60ab7a2af354ed86c63e
Micael <micaelsilveira31@gmail.com>
2021-12-22 11:14:14 -0300

    input button

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./Input.module.css";
-const Input = ({ label, type }) => {
+const Input = ({ label, type, name }) => {
   return (
-    <div>
-      <label>{label}</label>
-      <input className={styles.input} type={type} />;
+    <div className={styles.wrapper}>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
+      <input className={styles.input} type={type} id={name} />
+      <p className={styles.error}>Erro</p>
     </div>
   );
 };
