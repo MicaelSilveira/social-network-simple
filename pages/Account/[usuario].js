5fad48f85d630a880918260210c267475e3fa324
Micael <micaelsilveira31@gmail.com>
2021-12-23 22:49:31 -0300

    username

--- pages/Account/[usuario].js
+++ pages/Account/[usuario].js
@@ -1,9 +1,12 @@
 import React from "react";
-
+import Head from "next/head";
 const usuario = ({ data }) => {
   if (data)
     return (
       <div className="animeLeft">
+        <Head>
+          <title>{`Account | ${data.username}`}</title>
+        </Head>
         <h1>Bem vindo: {data.username}</h1>
       </div>
     );
