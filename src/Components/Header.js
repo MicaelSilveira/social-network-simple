f26449c60ac03a04cdd7bfd75e943dde5fea0b60
Micael <micaelsilveira31@gmail.com>
2021-12-23 21:22:02 -0300

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,20 +3,29 @@ import styles from "./Header.module.css";
 import React from "react";
 import Image from "next/image";
 import Logo from "../Assets/dogs.svg";
-const Header = () => {
+import Button from "./Forms/Button";
+
+const Header = ({ data, userLogout }) => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link href="/">
-          <a href="">
+          <a>
             <Image src={Logo} className={styles.logo} />
           </a>
         </Link>
-        <Link href="/User/Login-Home">
-          <a href="" className={styles.login}>
-            Login / Criar
-          </a>
-        </Link>
+        {data ? (
+          <Link href="/">
+            <a className={styles.login}>
+              <strong>@{data.nome}</strong>
+            </a>
+          </Link>
+        ) : (
+          <Link href="/User/Login-Home">
+            <a className={styles.login}>Login / Criar</a>
+          </Link>
+        )}
+        {data && <Button onClick={userLogout}>Sair</Button>}
       </nav>
     </header>
   );
