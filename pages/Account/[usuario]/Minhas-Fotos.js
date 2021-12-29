af2b73b1b5a8fe4bdc3cfdb0181523084a2d2203
Micael <micaelsilveira31@gmail.com>
2021-12-28 23:04:24 -0300

    to maluco

--- pages/Account/[usuario]/Minhas-Fotos.js
+++ pages/Account/[usuario]/Minhas-Fotos.js
@@ -1,19 +1,55 @@
 import React from "react";
-import Head from "next/head";
-import Feed from "../../../src/Components/Feed/Feed";
+import useFetch from "../../../src/Components/Hooks/useFetch";
+import { API_URL } from "../../../src/Components/api";
+import Error from "next/error";
 const MinhasFotos = ({ data }) => {
-  if (data) {
-    const userName = data.nome;
+  const userID = data.id;
+  const { dataFetch, error, loading, request } = useFetch();
+  function _GET({ page, total }) {
+    return {
+      url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${userID}`,
+      options: {
+        method: "GET",
+        cache: "no-store",
+      },
+    };
+  }
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = _GET({ page: 1, total: 5 });
+      console.log(url);
+      await request(url, options);
+    }
+    fetchPhotos();
+  }, []);
+
+  // React.useEffect(() => {
+  //   async function fetchPhotos() {
+  //     const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 6199 });
+  //     const { json } = await request(url, options);
+  //     console.log(json);
+  //   }
+  //   fetchPhotos();
+  // }, []);
+  if (error) <Error error={error} />;
+
+  if (dataFetch) {
+    console.log(dataFetch);
     return (
-      <div className="animeLeft">
-        <Head>
-          <title> @{userName}| Minhas Fotos</title>
-        </Head>
+      <>
         <h1 className="title">Minhas fotos</h1>
-        <section>
-          <Feed />
-        </section>
-      </div>
+        <ul className={`animeLeft`}>
+          <li></li>
+          {dataFetch.map((photo) => {
+            return (
+              <li key={photo.id}>
+                <img src={photo.src} alt={photo.id} />
+                <span>{photo.title}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </>
     );
   } else return null;
 };
