6c062ee6b04dd936c5937c3dc6345e6234a2bc16
Micael <micaelsilveira31@gmail.com>
2021-12-28 23:06:08 -0300

    parei

--- pages/Account/[usuario]/Minhas-Fotos.js
+++ pages/Account/[usuario]/Minhas-Fotos.js
@@ -1,55 +1,19 @@
 import React from "react";
-import useFetch from "../../../src/Components/Hooks/useFetch";
-import { API_URL } from "../../../src/Components/api";
-import Error from "next/error";
+import Head from "next/head";
+import Feed from "../../../src/Components/Feed/Feed";
 const MinhasFotos = ({ data }) => {
-  const userID = data.id;
-  const { dataFetch, error, loading, request } = useFetch();
-  function _GET({ page, total }) {
-    return {
-      url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${userID}`,
-      options: {
-        method: "GET",
-        cache: "no-store",
-      },
-    };
-  }
-  React.useEffect(() => {
-    async function fetchPhotos() {
-      const { url, options } = _GET({ page: 1, total: 5 });
-      console.log(url);
-      await request(url, options);
-    }
-    fetchPhotos();
-  }, []);
-
-  // React.useEffect(() => {
-  //   async function fetchPhotos() {
-  //     const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 6199 });
-  //     const { json } = await request(url, options);
-  //     console.log(json);
-  //   }
-  //   fetchPhotos();
-  // }, []);
-  if (error) <Error error={error} />;
-
-  if (dataFetch) {
-    console.log(dataFetch);
+  if (data) {
+    const userName = data.nome;
     return (
-      <>
+      <div className="animeLeft">
+        <Head>
+          <title> @{userName}| Minhas Fotos</title>
+        </Head>
         <h1 className="title">Minhas fotos</h1>
-        <ul className={`animeLeft`}>
-          <li></li>
-          {dataFetch.map((photo) => {
-            return (
-              <li key={photo.id}>
-                <img src={photo.src} alt={photo.id} />
-                <span>{photo.title}</span>
-              </li>
-            );
-          })}
-        </ul>
-      </>
+        <section>
+          <Feed />
+        </section>
+      </div>
     );
   } else return null;
 };
