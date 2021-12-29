81bb4f2d2901ee1405539acdd57a7ef93e98a56a
Micael <micaelsilveira31@gmail.com>
2021-12-28 22:51:19 -0300

    test img

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,16 +6,7 @@ import Error from "../Helper/Error";
 import styles from "./FeedPhotos.module.css";
 const FeedPhotos = () => {
   const userID = window.localStorage.getItem("userID");
-
-  React.useEffect(() => {
-    async function fetchPhotos() {
-      const { url, options } = _GET({ page: 1, total: 5 });
-      console.log(url);
-      const { json } = await request(url, options);
-      console.log(json);
-    }
-    fetchPhotos();
-  }, []);
+  const { dataFetch, error, loading, request } = useFetch();
   function _GET({ page, total }) {
     return {
       url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${userID}`,
@@ -25,7 +16,16 @@ const FeedPhotos = () => {
       },
     };
   }
-  const { dataFetch, error, loading, request } = useFetch();
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = _GET({ page: 1, total: 5 });
+      console.log(url);
+      const { json } = await request(url, options);
+      console.log(json);
+    }
+    fetchPhotos();
+  }, []);
+
   // React.useEffect(() => {
   //   async function fetchPhotos() {
   //     const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 6199 });
@@ -36,9 +36,9 @@ const FeedPhotos = () => {
   // }, []);
   if (error) <Error error={error} />;
   if (dataFetch) {
+    console.log(dataFetch);
     return (
       <ul className={`${styles.feed} animeLeft`}>
-        <li></li>
         {dataFetch.map((photo) => {
           return <FeedPhotoItem key={photo.id} photo={photo} />;
         })}
