b385cbd7e96187c7f6d1a8e538350d0451a37f36
Micael <micaelsilveira31@gmail.com>
2021-12-30 18:03:52 -0300

    css

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,16 +1,20 @@
 import Link from "next/link";
 import React from "react";
+import styles from "./PhotoContent.module.css";
 const PhotoContent = ({ data }) => {
   const { photo } = data;
   return (
-    <div className="animeLeft">
+    <div className={`${styles.photo} animeLeft`}>
       <div>
         <img src={photo.src} alt={photo.title} />
       </div>
       <div>
         <div>
-          <p>
-            <a>@{photo.author}</a>
+          <p className={styles.author}>
+            <Link href={`/Perfil/@${photo.author}`}>
+              <a>@{photo.author}</a>
+            </Link>
+            <span className={styles.visualizaçao}>{photo.acessos}</span>
           </p>
           <h1 className="title">
             <Link href={`/Foto/${photo.id}`}>
